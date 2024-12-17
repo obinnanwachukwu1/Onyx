1843cb5ad239fe9de4fa1d9994e4a5808e6e66e6
ChemicalDaniel <danielnwachukwu@outlook.com>
2024-12-17 02:22:45 -0500

    move CNAME to build

--- src/Apps/Resume/Resume.js
+++ src/Apps/Resume/Resume.js
@@ -1,11 +1,10 @@
 import React from 'react';
 import "./Resume.css"
-import resumePDF from "../../assets/content/resume.pdf"
 
 const Resume = () => {
     return (
         <div className="resume">
-            <object data={`${resumePDF}#zoom=100`} type="application/pdf" width="100%" height="100%"></object>
+            <object data="www.obinnanwachukwu.com/resume.pdf#zoom=100" type="application/pdf" width="100%" height="100%"></object>
         </div>
     );
 };
