4c730418dbf6bfe14c1c19507c5b22ca08f5994e
obinnanwachukwu1 <me@obinnanwachukwu.com>
2025-10-29 03:09:53 -0400

    Fix mobile app launcher

--- src/Components/WindowContext.ts
+++ src/Components/WindowContext.ts
@@ -5,9 +5,15 @@ import { AppManagerContext } from './AppManagerContext';
 
 export const useWindowContext = (): WindowContextValue => {
   const windowManagerContext = useContext(WindowManagerContext);
+  if (windowManagerContext !== undefined) {
+    return windowManagerContext;
+  }
+
   const appManagerContext = useContext(AppManagerContext);
 
-  const isInsideWindowManager = windowManagerContext !== undefined;
+  if (appManagerContext === undefined) {
+    throw new Error('useWindowContext must be used within a WindowManager or AppManager provider');
+  }
 
-  return isInsideWindowManager ? windowManagerContext : appManagerContext;
+  return appManagerContext;
 };
